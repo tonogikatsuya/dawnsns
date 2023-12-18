947cf8fdc01e92aa31a5cd0ca8e52d447c2c7d89
外木克弥 <tonogi_katsu@icloud.com>
2023-12-19 00:49:33 +0900

    フォローリスト作成中

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,18 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use DB;
 
 class FollowsController extends Controller
 {
     //
     public function followList(){
-        return view('follows.followList');
-        $follows = DB::table('folows')
+     $follows = DB::table('folows')
         ->join('users', 'follows.id', '=', 'users.id')
             ->select('follows.id', 'follows.id', 'follows.posts', 'follows.created_at', 'users.username', 'users.images')
             ->get();
-        return view('follows.index',['follows'=>$follows]);
+        return view('follows.followerList');
     }
+
     public function followerList(){
         return view('follows.followerList');
     }
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.login')
 
 @section('content')
-<form action="/follow-list" method="post">
+<form action="/follow-list" method="get">
   <input type="image" src="/images/{{ $follows->images}}" alt="アイコン">
   @csrf
-
 </form>
-@
+
+@foreach
+<table>
+  <td>
+    <img src="/images/{{ $post->images}}" alt="アイコン">
+  </td>
+  <td>{{ $follows->username }}</td>
+  <td>{{ $follows->posts }}</td>
+  <td>{{ $follows->created_at }}</td>
+</table>
 @endsection
